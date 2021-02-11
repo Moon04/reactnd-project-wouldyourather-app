98903cee56cd59d01bb9c7b7d033588c597c9ab3
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-11 22:43:12 +0200

    create helper file

---
+++ src/utils/helpers.js
@@ -0,0 +1,39 @@
+function generateUID() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
+export function formatDate(timestamp) {
+  const d = new Date(timestamp);
+  const time = d.toLocaleTimeString("en-US");
+  return time.substr(0, 5) + time.slice(-2) + " | " + d.toLocaleDateString();
+}
+
+export function formatQuestion({ optionOneText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    },
+  };
+}
+
+export function formatUser({id, name, avatarURL, answers, questions}) {
+  return {
+    id,
+    name,
+    avatarURL,
+    totalAnswered: Object.keys(answers).length,
+    totalCreated: questions.length,
+    totalScore: Object.keys(answers).length + questions.length
+  }
+}
