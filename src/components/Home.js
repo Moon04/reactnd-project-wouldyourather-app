fe7ebc3681a04a1aae5eccbf5266350867828907
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-12 01:05:54 +0200

    solve updating home issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,66 +1,56 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import Question from "./Question";
 
-class Home extends Component {
-  state = {
-    activeStatus: 1,
-    questionsIdsList: [],
-  };
+const Home = ({
+  unAnsweredQuestionsIds,
+  answeredQuestionsIds,
+}) => {
+  const [activeStatus, setActiveStatus] = useState(1);
+  const [questionsIdsList, setQuestionsIdsList] = useState([]);
 
-  componentDidMount() {
-    const { unAnsweredQuestionsIds } = this.props;
-    this.setState(() => ({
-      questionsIdsList: [...unAnsweredQuestionsIds],
-    }));
-  }
+  useEffect(() => {
+    console.log("blaaa");
+    setQuestionsIdsList([...unAnsweredQuestionsIds]);
+  }, [unAnsweredQuestionsIds]);
 
-  showQuestionsByStatus = (status) => {
-    const { unAnsweredQuestionsIds, answeredQuestionsIds } = this.props;
+  const showQuestionsByStatus = (status) => {
     if (status === 1) {
-      this.setState(() => ({
-        questionsIdsList: [...unAnsweredQuestionsIds],
-        activeStatus: status,
-      }));
+      setQuestionsIdsList([...unAnsweredQuestionsIds]);
     } else if (status === 2) {
-      this.setState(() => ({
-        questionsIdsList: [...answeredQuestionsIds],
-        activeStatus: status,
-      }));
+      setQuestionsIdsList([...answeredQuestionsIds]);
     }
+    setActiveStatus(status);
   };
 
-  render() {
-    const { questionsIdsList, activeStatus } = this.state;
-    return (
-      <div className="card w-50 mx-auto my-4">
-        <div className="card-header p-0">
-          <button
-            className={`btn font-weight-bold p-3 w-50 ${
-              activeStatus === 1 ? "text-info" : "bg-white"
-            }`}
-            onClick={() => this.showQuestionsByStatus(1)}
-          >
-            Unanswered Questions
-          </button>
-          <button
-            className={`btn font-weight-bold p-3 w-50 ${
-              activeStatus === 2 ? "text-info" : "bg-white"
-            }`}
-            onClick={() => this.showQuestionsByStatus(2)}
-          >
-            Answered Questions
-          </button>
-        </div>
-        <div className="card-body">
-          {questionsIdsList?.map((questionId) => (
-            <Question key={questionId} questionId={questionId} />
-          ))}
-        </div>
+  return (
+    <div className="card w-50 mx-auto my-4">
+      <div className="card-header p-0">
+        <button
+          className={`btn font-weight-bold p-3 w-50 ${
+            activeStatus === 1 ? "text-info" : "bg-white"
+          }`}
+          onClick={() => showQuestionsByStatus(1)}
+        >
+          Unanswered Questions
+        </button>
+        <button
+          className={`btn font-weight-bold p-3 w-50 ${
+            activeStatus === 2 ? "text-info" : "bg-white"
+          }`}
+          onClick={() => showQuestionsByStatus(2)}
+        >
+          Answered Questions
+        </button>
       </div>
-    );
-  }
-}
+      <div className="card-body">
+        {questionsIdsList?.map((questionId) => (
+          <Question key={questionId} questionId={questionId} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 function mapStateToProps({ questions, authedUser }) {
   return {
